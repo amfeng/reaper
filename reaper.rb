1203d44d0bf1acc3030871f18baa7be1e0d62cd6
Amber Feng <amber.feng@gmail.com>
2014-10-25 14:54:31 -0700

    Flesh out rest of issue functionality, add welcome.

--- reaper.rb
+++ reaper.rb
@@ -1,24 +1,23 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require 'octokit'
 require 'colorize'
 require 'time'
-require 'pry'
+require 'singleton'
 
 class Reaper
   STALE_THRESHOLD = 3600 * 24 * 30 * 3 # 3 months
 
   def initialize
-    # Look for .netrc credentials first; if they don't exist then prompt
-    # for login.
-    # TODO
-    @client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+    @client = Client.instance
   end
 
   def run!
-    repo_path = 'amfeng/todidnt'
-
-    puts "=== Reaping: #{repo_path}".bold.white
-
+    repo_path = 'amfeng/reaper'
     now = Time.now
+
+    puts "Welcome to Reaper! ⟝⦆ (fetching from `#{repo_path}`)".white.bold
 
     # Fetch issues in ascending updated order.
     options = {
@@ -26,16 +25,15 @@ class Reaper
       direction: :asc
     }
 
-    @client.list_issues(repo_path, options).each do |issue|
+    issues = @client.list_issues
+    issues_reaped = false
+
+    issues.each do |issue|
       issue = Issue.new(issue)
 
       # Skip if this task has already been closed or has a do-not-reap tag.
       next if issue.closed?
       next if issue.labels.include?('do-not-reap')
-
-      # Break out of the whole loop if the issue's updated date is outside
-      # the range.
-      break if issue.updated_at > now - STALE_THRESHOLD
 
       # If there's a to-reap tag, close it. Else, add a to-reap tag and
       # a warning comment.
@@ -43,55 +41,114 @@ class Reaper
       # customizable and not based on run-time of the reaper (e.g. running
       # reaper should be idempotent).
 
+      puts "\n"
       if issue.labels.include?('to-reap')
+        issues_reaped = true
         # TODO: Add force-all flag.
 
-        puts "Issue ##{issue.number}: #{issue.title}"
+        puts "= Issue ##{issue.number}: #{issue.title}".white
         print "Close issue? [Y]es, [N]o, or n[E]ver: ".yellow
         input = $stdin.gets.chomp.downcase
         case input
         when 'y'
-          #issue.close
+          issue.close
           puts "Issue was reaped.".green
         when 'n'
           puts "OK, skipping.".red
         when 'e'
-          #issue.labels << 'do-not-reap'
-          #issue.save
+          issue.labels << 'do-not-reap'
+          issue.labels -= ['to-reap']
+          issue.save
           puts "OK, added `do-not-reap`.".green
         end
-
       else
-        #issue.labels << 'to-reap'
-        #issue.comment("reaper warning")
-        #issue.save
-        puts "Added `to-reap` to #{issue.number}"
+        # Break out of the whole loop if the issue's updated date is outside
+        # the range.
+        break if issue.updated_at > now - STALE_THRESHOLD
+
+        issues_reaped = true
+        puts "= Issue ##{issue.number}: #{issue.title}".white
+        print "Add warning? [Y]es, [N]o, or n[E]ver: ".yellow
+        input = $stdin.gets.chomp.downcase
+
+        case input
+        when 'y'
+          issue.labels << 'to-reap'
+          issue.comment("reaper warning")
+          issue.save
+          puts "Added `to-reap` to #{issue.number}"
+        when 'n'
+          puts "OK, skipping.".red
+        when 'e'
+          issue.labels << 'do-not-reap'
+          issue.labels -= ['to-reap']
+          issue.save
+          puts "OK, added `do-not-reap`.".green
+        end
       end
+    end
+
+    unless issues_reaped
+      puts "No reap-able issues, woohoo!".green
+    end
+  end
+
+  class Client
+    include Singleton
+
+    attr_accessor :client
+
+    def initialize
+      # Look for .netrc credentials first; if they don't exist then prompt
+      # for login.
+      # TODO
+      @client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+      @repo = 'amfeng/reaper'
+
+      # TODO: Add things other than Github.
+    end
+
+    def method_missing(meth, *args, &block)
+      @client.send(meth, *([@repo] + args), &block)
     end
   end
 
   class Issue
-    attr_accessor :labels
+    attr_accessor :state, :title, :body, :labels
 
     def initialize(issue)
       @issue = issue
+
+      @title = @issue.title
+      @body = @issue.body
+      @state = @issue.state
       @labels = issue.labels.map(&:name)
+      @buffered_comments = []
+
+      @client = Client.instance
     end
 
     def close
-      # TODO
+      @labels << 'reaped'
+      @labels -= ['to-reap']
+      @client.close_issue(@issue.number, labels: @labels)
     end
 
     def closed?
-      @issue.state == 'closed'
+      @state == 'closed'
     end
 
     def comment(comment)
-      # TODO
+      @buffered_comments << comment
     end
 
     def save
-      # TODO
+      @client.update_issue(@issue.number, @title, @body, labels: @labels)
+
+      @buffered_comments.each do |comment|
+        @client.add_comment(@issue.number, comment)
+      end
+      @buffered_comments = []
     end
 
     def method_missing(meth, *args, &block)
