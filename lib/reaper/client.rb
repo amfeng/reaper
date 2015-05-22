a137a2010c1592082dcccdfb34c7ff4c8460a6a3
Amber Feng <amber@stripe.com>
2015-05-22 11:58:15 -0700

    Error on missing access token.

--- lib/reaper/client.rb
+++ lib/reaper/client.rb
@@ -11,7 +11,11 @@ module Reaper
       # Look for .netrc credentials first; if they don't exist then prompt
       # for login.
       # TODO
-      @client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+      access_token = ENV['GITHUB_ACCESS_TOKEN']
+
+      raise "Missing Github access token." if access_token.nil?
+
+      @client = Octokit::Client.new(access_token: access_token)
       @repo = nil
 
       # TODO: Add things other than Github.
